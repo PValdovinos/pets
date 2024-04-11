9cc9e1bc90ccd05294ed88c1cae67b7485031f96
Pedro V.R. <ptreyes2@gmail.com>
2024-04-11 13:33:17 -0400

    Added home page view

--- index.php
+++ index.php
@@ -15,11 +15,11 @@ $f3 = Base::instance();
 //define a default route
 $f3->route('GET /', function(){
 
-    echo '<h1>Pet Home</h1>';
+    //echo '<h1>Pet Home</h1>';
 
     //render a view page
-    //$view = new Template();
-    //echo $view->render('views/home.html');
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 // Run Fat-Free
